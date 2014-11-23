e083418efd4beada5b2cdb69bf92c63a695ec6ba
Jake Champlin <jake.champlin.27@gmail.com>
2014-11-23 02:37:24 -0500

    Change Emacs theme to monokai theme

--- init/init-appearance.el
+++ init/init-appearance.el
@@ -1,10 +1,12 @@
 (provide 'init-appearance)
 
-(quelpa '(color-theme-solarized
-	  :repo "fujin/emacs-color-theme-solarized"
-	  :fetcher github))
+;(quelpa '(color-theme-solarized
+;	  :repo "fujin/emacs-color-theme-solarized"
+;	  :fetcher github))
 
+(quelpa 'monokai-theme)
 (quelpa 'dash)
 (require 'dash)
 
-(load-theme 'solarized-dark t)
+;(load-theme 'solarized-dark t)
+(load-theme 'monokai t)
