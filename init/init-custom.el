5414f5950f3e151f6a4db85b4e08cb9a9fadabec
Jake Champlin <jake.champlin.27@gmail.com>
2015-01-18 04:05:40 -0500

    Update for Mouse Mode in emacsclient

--- init/init-custom.el
+++ init/init-custom.el
@@ -59,3 +59,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Mouse Mode for Emacs Client
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
+(setq mouse-sel-mode t)
